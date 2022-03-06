7086ac9d76c70a2f608881f0134fb461a8031631
limheonjun <jumong40@naver.com>
2022-03-06 21:50:25 +0900

    feat: Bean Validation

--- src/test/unit/com/kotlinspring/controller/CourseControllerUnitTest.kt
+++ src/test/unit/com/kotlinspring/controller/CourseControllerUnitTest.kt
@@ -5,26 +5,36 @@ import com.kotlinspring.entity.Course
 import com.kotlinspring.service.CourseService
 import com.kotlinspring.util.courseDTO
 import io.mockk.every
-import io.mockk.runs
-import io.mockk.verify
-import jdk.internal.vm.compiler.word.LocationIdentity.any
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.mockkStatic
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.context.annotation.Bean
 import org.springframework.test.web.reactive.server.WebTestClient
+import java.lang.RuntimeException
 
 @WebMvcTest(CourseController::class)
 @AutoConfigureWebTestClient
 class CourseControllerUnitTest {
 
+    @TestConfiguration
+    class ControllerTestConfig {
+        @Bean
+        fun courseServiceMock() = mockk<CourseService>()
+    }
+
     @Autowired
     lateinit var webTestClient: WebTestClient
 
-    @MockBean
+    @Autowired
     lateinit var courseServiceMock : CourseService
 
     @Test
@@ -47,6 +57,44 @@ class CourseControllerUnitTest {
         Assertions.assertTrue {
             savedCourseDto!!.id != null
         }
+    }
+
+    @Test
+    fun addCourse_validation() {
+        val courseDTO = CourseDTO(null, "", "")
+        every { courseServiceMock.addCourse(any()) } returns courseDTO(id = 1)
+
+        val response = webTestClient
+            .post()
+            .uri("/v1/courses")
+            .bodyValue(courseDTO)
+            .exchange()
+            .expectStatus().isBadRequest
+            .expectBody(String::class.java)
+            .returnResult()
+            .responseBody
+
+        assertEquals("courseDTO.category must not be blank, courseDTO.name must not be blank", response)
+    }
+
+    @Test
+    fun addCourse_runtimeException() {
+        val courseDTO = CourseDTO(null, "Build Kotlin Api", "hj")
+        val errorMessage = "Unexpected Error occurred"
+        every { courseServiceMock.addCourse(any()) } throws RuntimeException(errorMessage)
+
+
+        val response = webTestClient
+            .post()
+            .uri("/v1/courses")
+            .bodyValue(courseDTO)
+            .exchange()
+            .expectStatus().is5xxServerError
+            .expectBody(String::class.java)
+            .returnResult()
+            .responseBody
+
+        assertEquals(errorMessage, response)
     }
 
     @Test
